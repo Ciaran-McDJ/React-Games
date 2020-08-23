d2b602d8b2db1f7f8e7ffff930c48c3f43c18608
Tadhg McDonald-Jensen <morningninjamcdj@gmail.com>
2020-08-22 21:16:52 -0400

    One aspect of configs changeable

--- src/game/Chase_The_Sandwich/game3.tsx
+++ src/game/Chase_The_Sandwich/game3.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React, { createRef, useReducer } from "react";
 import { HookedComponent } from "../../components/hooklib";
 import { useThing } from "../../Homemade Functions/get min size";
 import { Player } from "./Player";
@@ -20,53 +20,73 @@ export function ChaseTheSandwich() {
     );
     let [score, changeScore] = React.useState(0);
     let [gameOver, changeGameOver] = React.useState(false);
-    let [highScore, changeHighScore] = React.useState(0)
+    let [highScore, changeHighScore] = React.useState(0);
     //state variables for changing config
-    let [newPlayerRadius, changeNewPlayerRadius] = React.useState(String(gameConfig.playerRadius))
+    //let [new, changeNew] = React.useState(String(gameConfig.))
+    let [newPlayerRadius, changeNewPlayerRadius] = React.useState(
+        String(gameConfig.playerRadius)
+    );
+    type ConfigAsStrings = Record<keyof typeof gameConfig, number | string>;
+    let [newConfig, changeNewConfig] = React.useReducer(
+        (prevState: ConfigAsStrings, arg: Partial<ConfigAsStrings>) => ({ ...prevState, ...arg }),
+        gameConfig
+    );
+    // The up to date config that game is providing to children
+    let [actualConfig, changeActualConfig] = React.useState(gameConfig);
 
-    useInterval(() => update(1 / gameConfig.fps), 1000 / gameConfig.fps);
+    useInterval(() => update(1 / actualConfig.fps), 1000 / actualConfig.fps);
 
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignSelf: "center",
-                flexGrow: 1,
-            }}
-        >
-            highscore: {highScore}
-            <br/>
-            score: {score}
-            <br/>
-            {gameOver == false
-                ? "You didn't die yet, good job :)"
-                : "YOU DIED!!!!! :("}
-            <br/>
-            <br/>
-            Configurations below:
-            <input value={newPlayerRadius} onChange={(newInput)=>{changeNewPlayerRadius(newInput.target.value)}}></input>
-            <br/>
-            <button onClick={restart}>restart</button>
-            <svg
+        <gameConfigContext.Provider value={actualConfig}>
+            <div
                 style={{
+                    display: "flex",
+                    flexDirection: "column",
                     alignSelf: "center",
-                    width: gameConfig.gameSize,
-                    height: gameConfig.gameSize,
-                    borderWidth: 10,
-                    borderColor: "red",
-                    backgroundColor: gameOver==true?"lightgreen":"white",
-                    borderStyle: "solid",
+                    flexGrow: 1,
                 }}
             >
-                <Player ref={playerRef} color={"red"} />
-                <Sandwich ref={sandwichRef} color="blue" />
+                highscore: {highScore}
+                <br />
+                score: {score}
+                <br />
+                {gameOver == false
+                    ? "You didn't die yet, good job :)"
+                    : "YOU DIED!!!!! :("}
+                <br />
+                <br />
+                Configurations below:
+                <input
+                    // value={newPlayerRadius}
+                    // onChange={(newInput) => {
+                    //     changeNewPlayerRadius(newInput.target.value);
+                    // }}
+                    value={newConfig.ballRadius}
+                    onChange={(newInput) => changeNewConfig({ballRadius:newInput.target.value})}
+                ></input>
+                <br />
+                <button onClick={restart}>restart</button>
+                <svg
+                    style={{
+                        alignSelf: "center",
+                        width: actualConfig.gameSize,
+                        height: actualConfig.gameSize,
+                        borderWidth: 10,
+                        borderColor: "red",
+                        backgroundColor:
+                            gameOver == true ? "lightgreen" : "white",
+                        borderStyle: "solid",
+                    }}
+                >
+                    <Player ref={playerRef} color={"red"} />
+                    <Sandwich ref={sandwichRef} color="blue" />
 
-                {refsOfBalls.map((ref) => (
-                    <BallOfDeath ref={ref} color="black" />
-                ))}
-            </svg>
-        </div>
+                    {refsOfBalls.map((ref) => (
+                        <BallOfDeath ref={ref} color="black" />
+                    ))}
+                </svg>
+            </div>
+        </gameConfigContext.Provider>
     );
     function summonBallOfDeath() {
         changeRefsOfBalls([...refsOfBalls, createRef()]);
@@ -75,8 +95,8 @@ export function ChaseTheSandwich() {
     function deathCollisionCheck(ballRef: React.RefObject<BallOfDeath>) {
         if (
             ballCollisionCheck(
-                gameConfig.ballRadius,
-                gameConfig.playerRadius,
+                actualConfig.ballRadius,
+                actualConfig.playerRadius,
                 ballRef.current!.ballX,
                 playerRef.current!.playerX,
                 ballRef.current!.ballY,
@@ -85,25 +105,36 @@ export function ChaseTheSandwich() {
         ) {
             changeGameOver(true);
         }
+        // for(let idx = 0; idx < 8; idx+=1){
+
+        // }
     }
 
-    function restart() {
-        changeGameOver(false)
-        changeRefsOfBalls([])
-        changeScore(0)
+    
 
+    function restart() {
+        changeGameOver(false);
+        changeRefsOfBalls([]);
+        changeScore(0);
+        //Config that I will change all the properties from strings to numbers
+        let numConfig = {} as typeof gameConfig;
+        let configProperties = Object.keys(newConfig)
+        for (let aspect of configProperties) {
+            numConfig[aspect] = Number(newConfig [aspect])
+        }
+        changeActualConfig(numConfig);
     }
 
     function update(time: number) {
-        playerRef.current!.movePlayer(1 / gameConfig.fps);
+        playerRef.current!.movePlayer(1 / actualConfig.fps);
         for (let ref of refsOfBalls) {
             ref.current!.moveBall(time);
             deathCollisionCheck(ref);
         }
         if (
             ballCollisionCheck(
-                gameConfig.sandwichRadius,
-                gameConfig.playerRadius,
+                actualConfig.sandwichRadius,
+                actualConfig.playerRadius,
                 sandwichRef.current!.sandwichX,
                 playerRef.current!.playerX,
                 sandwichRef.current!.sandwichY,
@@ -111,11 +142,14 @@ export function ChaseTheSandwich() {
             )
         ) {
             summonBallOfDeath();
-            sandwichRef.current!.repositionSandwich(playerRef.current!.playerX, playerRef.current!.playerY);
+            sandwichRef.current!.repositionSandwich(
+                playerRef.current!.playerX,
+                playerRef.current!.playerY
+            );
             if (gameOver == false) {
                 changeScore(score + 1);
-                if (score+1>highScore) {
-                    changeHighScore(score+1)
+                if (score + 1 > highScore) {
+                    changeHighScore(score + 1);
                 }
             }
         }
